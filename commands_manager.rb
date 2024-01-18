1332f5d7808106cb6efa9a73be953ab7f3eb7d26
Andrii Nikolaiev <andrii.nikolaiev@thirdspacelearning.com>
2024-01-18 21:50:34 +0000

    make commands responses ephemeral

--- commands_manager.rb
+++ commands_manager.rb
@@ -8,7 +8,7 @@ class CommandsManager
     bot.application_command(:setup) do |event|
       server = Server.new(event.server)
       server.setup(event.options)
-      event.respond(content: "Guild Name: #{server.guild_name}\nOfficer Role ID: #{server.officer_role_id}")
+      event.respond(content: "Guild Name: #{server.guild_name}\nOfficer Role ID: #{server.officer_role_id}", ephemeral: true)
     end
 
     bot.register_application_command(:info, 'Show setup details', server_id: '697004853494546473') do |cmd|
@@ -16,7 +16,7 @@ class CommandsManager
 
     bot.application_command(:info) do |event|
       server = Server.new(event.server)
-      event.respond(content: "Guild Name: #{server.guild_name}\nOfficer Role ID: #{server.officer_role_id}")
+      event.respond(content: "Guild Name: #{server.guild_name}\nOfficer Role ID: #{server.officer_role_id}", ephemeral: true)
     end
 
     bot.register_application_command(:'add-guild-to-whitelist', 'Whitelist a guild') do |cmd|
@@ -26,7 +26,7 @@ class CommandsManager
     bot.application_command(:'add-guild-to-whitelist') do |event|
       server = Server.new(event.server)
       guilds = server.whitelist_guild(event.options['guild'])
-      event.respond(content: guilds.join(', '))
+      event.respond(content: guilds.join(', '), ephemeral: true)
     end
 
     bot.register_application_command(:'remove-guild-from-whitelist', 'Remove guild from a whitelist') do |cmd|
@@ -36,7 +36,7 @@ class CommandsManager
     bot.application_command(:'remove-guild-from-whitelist') do |event|
       server = Server.new(event.server)
       guilds = server.remove_guild_from_whitelist(event.options['guild'])
-      event.respond(content: guilds.join(', '))
+      event.respond(content: guilds.join(', '), ephemeral: true)
     end
   end
 end
